692c89d1fae5a0648336aa664d1b53d636fd3893
jawahirkashim <jawahir.abul2007@gmail.com>
2014-02-15 22:54:11 +0530

    error are removed

--- easy/SumPrime.java
+++ easy/SumPrime.java
@@ -1,27 +1,29 @@
 package easy;
+
 //Sum of first 1000 primes
 public class SumPrime {
 public static void main(String[]args){
-	int num = 10;
-	int sum=0;
-	int[] aprime=getPrime(num);
-	for (int i : aprime) {
-		sum = sum + i;
+int sum=0;
+int count=0;
+int current=2;
+
+while(count<1000){
+	if(isPrime(current)){
+		sum=sum+current;
+		count++;
 	}
-	System.out.println("the sum of the prime till 1000 is : "+ sum);
+	current++;
 }
 
-private static int[] getPrime(int num) {
-	int[] anum= new int[num];
-	for (int i = 0; i < anum.length; i++) {
-		anum[i]=i;
-	}
-	anum[1]=0;
-	for (int i = 2; i < anum.length; i++) {
-		for (int j = 2; j <= (anum.length-1)/i; j++) {
-			anum[i*j]=0;
-		}
-	}
-	return anum;
+System.out.println(sum);
 }
+private static boolean isPrime(int num){
+	if(num<2)return false;
+	if(num==2||num==3)return true;
+	if(num%2==0||num%3==0)return false;
+	for (int i = 3; i*i <= num; i +=2) {
+		if(num%i==0){return false;}
+	}	
+	return true;
+	}
 }
